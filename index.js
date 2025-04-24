b053f25e02abb8023bd9f9a61697adb1207adceb
ron <ron.bachar@outlook.com>
2025-04-24 10:38:43 +0300

    Update Puppeteer configuration for better headless environment support

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-const { Client } = require('whatsapp-web.js');
 
-const client = new Client();
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu'
+        ],
+        headless: true
+    }
+});
 
 client.on('qr', qr => {
     qrcode.generate(qr, { small: true });
